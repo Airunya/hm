615d0db21eb72ddf25ada6ad0595d87932168ff4
Airunya <atatushkitatata@gmail.com>
2021-02-11 16:34:03 +0300

    added home made lesson_1

---
+++ Project/Lesson_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"Привет {name} сегодня {date.ToShortDateString()} и время {date.ToShortTimeString()}");
+            Console.ReadKey();
+         
+        }
+    }
+}
